cf94b6cace1bca786669ed4fb22a6c2481648efd
grpawel <grocholapawel@gmail.com>
2019-06-08 18:16:38 +0200

    Change color ordering (#31)
    * Reuse color after user disconnets
    
    * Fix combining colors
    
    * Fix sometimes returning duplicate colors

---
+++ server/src/main/java/pl/edu/agh/toik/infun/services/IRandomColor.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.toik.infun.services;
+
+public interface IRandomColor {
+    String getColor(int num);
+
+    void returnColor(String color);
+}
--- server/src/main/java/pl/edu/agh/toik/infun/services/RandomColor.java
+++ server/src/main/java/pl/edu/agh/toik/infun/services/RandomColor.java
@@ -1,12 +1,11 @@
 package pl.edu.agh.toik.infun.services;
 
-import org.springframework.stereotype.Service;
-
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
-@Service
-public class RandomColor {
+public class RandomColor implements IRandomColor {
     // from https://stackoverflow.com/a/4382138
     private static List<String> colors = Arrays.asList(
             "#FFB300", // Vivid Yellow
@@ -33,12 +32,24 @@ public class RandomColor {
             "#232C16" // Dark Olive Green
     );
 
-    public String getColor(final int num) {
-        if(num < colors.size()) {
+    private final Set<String> usedColors = ConcurrentHashMap.newKeySet();
+
+    public String getColor(int num) {
+        String color;
+        do {
+            color = nextColor(num);
+            num++;
+        } while(usedColors.contains(color));
+        usedColors.add(color);
+        return color;
+    }
+
+    private String nextColor(final int num) {
+        if (num < colors.size()) {
             return colors.get(num);
         }
-        final int colorIndex = num % colors.size();
-        final int indexDiff = num / colors.size();
+        final int colorIndex = num / colors.size();
+        final int indexDiff = num % colors.size();
         final String color1 = colors.get(colorIndex);
         final String color2 = colors.get((colorIndex + indexDiff) % colors.size());
         return combineColors(color1, color2);
@@ -48,5 +59,9 @@ public class RandomColor {
         final int num1 = Integer.parseInt(color1.substring(1), 16);
         final int num2 = Integer.parseInt(color2.substring(1), 16);
         return "#" + Integer.toHexString(num1 ^ num2);
+    }
+
+    public void returnColor(final String color) {
+        this.usedColors.remove(color);
     }
 }
